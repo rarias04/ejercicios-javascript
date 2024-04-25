e1321e42d061ee35506f6b86d7757fe25fe6bd9d
rarias04 <161629600+rarias04@users.noreply.github.com>
2024-04-25 21:33:47 +0200

    Create e2-guardar-informacion
    Create e2-guardar-informacion as html y js documents

---
+++ js/e2-guardar-informacion.js
@@ -0,0 +1,44 @@
+
+//Nombre de variable recomendado en minuscula
+let nombre;
+//Nombre de variable si tiene espacio, usar camelCase
+let myName;
+//Podeis usar guiones para separar los espacios pero es mas recomendable camelCase
+let mi_nombre;
+//Los nombres de las variables no pueden comenzar por un numero 2nombre
+//Los nombres de las variables  pueden tener numero siempre y cuando empiece por el
+let nombre2;
+
+
+//2.Iniciar una variable
+//Primero declarar la variable y luego iniciarla
+document.write("Ejemplo 1: Iniciar una variable </br>");
+let ciudad;
+ciudad = "Malaga";
+document.write("La variable ciudad tienen el valor" + ciudad + "</br>");
+//Declarar la variable en iniciarla a la vez
+document.write("Ejemplo 2: Iniciar una variable edad </br>");
+let edad = 33;
+document.write("La variable edad tiene el valor: " + edad + "</br>");
+
+//Tipos de variables
+//Numero entero o con decimales. Los decimales van con punto y siempre sin comillas
+document.write("Ejemplo 3: Iniciar una variable tipo numero </br>");
+let numero = 5;
+let numeroDecimales= 5.5;
+document.write("El numero sin decimales es: " + numero + "</br>");
+//cadenas. Pueden usarse comillas simples o dobles. No las mezcles, usa solo una de ellas. Cuidado con caracter especiales, no pueden ir comillas iguales dentro del texto.
+let saludo = "Hola que tal";
+let despedida = 'Adios, me voy';
+let numeroEnTexto = "33";
+//Booleanos. true o false. En minuscula y sin comillas
+document.write("Ejemplo 7: Crear una variable de tipo Booleano </br>");
+let estaSoleado = true;
+let esDeDia = false;
+document.write("La variable booleana esta soleado es: " + estaSoleado);
+document.write("</br>");
+document.write("La variable booleana es de dia es: " + esDeDia);
+
+
+
+
