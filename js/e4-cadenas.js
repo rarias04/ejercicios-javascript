015c8db7971821868bcf529ed9699151ffdf18f0
rarias04 <161629600+rarias04@users.noreply.github.com>
2024-04-30 15:52:13 +0200

    Created e4-cadenas HTML and JS files
    Created e4-cadenas HTML and JS files

---
+++ js/e4-cadenas.js
@@ -0,0 +1,6 @@
+// CADENAS (TEXTOS)
+// 1. Crea una variable con la frase Hola que tal, bienvenida a QA, y muestrala junto con:
+// su longitud
+let frase = "Hola que tal, bienvenida a QA";
+let longitud = frase.length;
+document.write("Longitud es " + longitud);
